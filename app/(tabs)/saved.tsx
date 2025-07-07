f5f51b52208e616d4cd70fc6719744659b2cde2d
Edson J Vargas <edson.jhair@yahoo.com>
2025-07-07 14:11:20 -0500

    JSMAstery - React native course - updating knoledge

---
+++ app/(tabs)/saved.tsx
@@ -0,0 +1,15 @@
+import { icons } from '@/constants/icons';
+import { Image, Text, View } from 'react-native';
+
+const Saved = () => {
+  return (
+    <View className='bg-primary flex-1 px-10'>
+      <View className='flex justify-center items-center flex-1 flex-col gap-5'>
+        <Image source={icons.person} className='size-10' tintColor='#FFF' />
+        <Text className='text-gray-500 text-base'>Saved</Text>
+      </View>
+    </View>
+  );
+};
+
+export default Saved;
